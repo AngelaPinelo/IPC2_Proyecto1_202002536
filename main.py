468d057f6fc7292bce3e2f9284345352e8407469
AngelaPinelo <78389599+AngelaPinelo@users.noreply.github.com>
2022-03-02 23:31:22 -0600

    insertando nodos piso

--- main.py
+++ main.py
@@ -1,18 +1,37 @@
 import xml.etree.ElementTree as ET
+from PisosList import ListaPisos
+
+lista_pisos=ListaPisos()
 
 
 def elementTree(ruta):
     tree = ET.parse(ruta)
     piso = tree.getroot()
+   #para mi nodo piso
     for n in piso:
+        nombre= n.attrib['nombre']
+        name=nombre
+        row= n[0]
+        r=row.text
+        r=int(r)
+        column= n[1]
+        c=column.text
+        c=int(c)
+        flip=n[2]
+        f=flip.text
+        f=int(f)
+        slide=n[3]
+        s=slide.text
+        s=int(s)
+        pisoNode=lista_pisos.insertNodeLast(name,r,c,f,s)
+        #para mi nodo patron
         patrones= n[4]
-        print(n.attrib['nombre'])
-        for r in n:
-            print(r.text)
         for patron in patrones:
-            print(patron.attrib['codigo'])
-            print(patron.text)
-
+            code=patron.attrib['codigo']
+            cadena=patron.text
+            print(code)
+            print(cadena)
+        
 
     
 elementTree('pisos.xml')
